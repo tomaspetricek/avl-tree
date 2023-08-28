a5afae8bfb637d09213b15c790bb464bbdfbd70c
TomasPetricek <45512445+tomaspetricek@users.noreply.github.com>
2023-08-28 19:23:40 +0200

    Implement insert and contains

--- include/avl_tree.hpp
+++ include/avl_tree.hpp
@@ -5,9 +5,89 @@
 #ifndef AVL_TREE_AVL_TREE_HPP
 #define AVL_TREE_AVL_TREE_HPP
 
-namespace top {
-    class avl_tree {
+#include <memory>
+#include <iostream>
 
+namespace top {
+    template<class T>
+    class avl_tree {
+        struct node {
+            T value;
+            node* right{nullptr}, * left{nullptr};
+        };
+        node* root_{nullptr};
+
+        void clear(node*& curr)
+        {
+            if (curr) {
+                clear(curr->left);
+                clear(curr->right);
+                delete curr;
+                curr = nullptr;
+            }
+        }
+    public:
+        using value_type = T;
+
+        void insert(const T& value)
+        {
+            if (!root_) {
+                root_ = new node{value};
+            }
+            else {
+                node* curr{root_}, * prev{nullptr};
+                while (curr) {
+                    prev = curr;
+                    if (curr->value==value) {
+                        return;
+                    }
+                    if (curr->value>value) {
+                        curr = curr->left;
+                    }
+                    else {
+                        curr = curr->right;
+                    }
+                }
+                if (prev->value>value) {
+                    prev->left = new node{value};
+                }
+                else {
+                    prev->right = new node{value};
+                }
+            }
+        }
+
+        bool contains(const T& value) const
+        {
+            node* curr{root_};
+            while (curr) {
+                if (curr->value==value) {
+                    return true;
+                }
+                if (curr->value>value) {
+                    curr = curr->left;
+                }
+                else {
+                    curr = curr->right;
+                }
+            }
+            return false;
+        }
+
+        bool empty() const
+        {
+            return !root_;
+        }
+
+        void clear()
+        {
+            clear(root_);
+        }
+
+        ~avl_tree()
+        {
+            clear(root_);
+        }
     };
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 #include <avl_tree.hpp>
+#include <cassert>
 
 int main()
 {
-    top::avl_tree tree;
+    using value_type = int;
+    top::avl_tree<value_type> tree;
+    tree.insert(7);
+    tree.insert(20);
+    tree.insert(5);
+    tree.insert(15);
+    tree.insert(10);
+    tree.insert(4);
+    tree.insert(4);
+    tree.insert(33);
+    tree.insert(2);
+    tree.insert(25);
+    tree.insert(6);
+    assert(!tree.contains(100));
+    assert(tree.contains(6));
+    assert(!tree.empty());
+    tree.clear();
+    assert(tree.empty());
     return 0;
 }
